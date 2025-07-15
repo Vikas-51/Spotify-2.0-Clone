e2d8c0bdb7b3b36a23fab903da4b37e14890524d
Vikas Prajapat <vikasprajapat010@gmail.com>
2025-07-15 20:01:21 +0530

    Add new project files after cleanup

--- spotify-2.0-clone/src/context/StateProvider.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useReducer } from "react";
-import { StateContext } from "./StateContext";
-
-export const StateProvider = ({ initialState, reducer, children }) => (
-  <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {children}
-  </StateContext.Provider>
-);
